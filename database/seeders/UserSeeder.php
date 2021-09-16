b2c1e67f9db6ff838bff695abb813c8c60325289
TheFnX <beto.dubal19@gmail.com>
2021-09-16 18:30:01 -0400

    1er commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::create([
+            'name' => 'Beto Dubal',
+            'email' => 'beto.dubal@gmail.com',
+            'password' => bcrypt('123456789')
+        ]);
+        // ->assignRole('Administrador')
+        User::factory(49)->create();
+    }
+}
